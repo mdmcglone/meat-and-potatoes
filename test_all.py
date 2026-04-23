8d2869c3ea4cd5a460757cf480efc4799ee20aaf
emdmcglone <emdmcglone@gmail.com>
2026-04-23 16:06:33 +0530

    testing suite

--- test_all.py
+++ test_all.py
@@ -15,6 +15,18 @@ def test_img_to_graph():
     assert os.path.getmtime("test_imgs/graph_pixels.png") > time.time() - 1
     assert os.path.getmtime("test_imgs/graph.png") > time.time() - 1
     
+def test_img_to_graph_error():
+    from graph_to_times import image_to_graph
+    
+    with pytest.raises(FileNotFoundError):
+        image_to_graph(
+            image_path="test_imgs/DNE.png",
+            show_plot=False,  # do not display; just save plots
+            save_raw_plot_path="test_imgs/graph_pixels.png",
+            save_filtered_plot_path="test_imgs/graph.png"
+        )
+        
+    
 @pytest.mark.parametrize("time_text,res, error", [
     ("3:4", 184, None),
     ("3:4:5", (3*3600) +(4*60) + 5, None),
@@ -31,4 +43,7 @@ def test_parse_time_to_seconds(time_text, res, error):
             parse_time_to_seconds(time_text)
     else:
         assert parse_time_to_seconds(time_text) == res
+        
+        
+
 
